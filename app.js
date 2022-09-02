8f384793183c91e7513e34d3b984da7c0182d71a
JavierHornos <hjavih@gmail.com>
2022-09-02 01:39:49 -0300

    Primer commit MLiebre

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicFolderPath = path.resolve(__dirname, './public');
+app.use( express.static(publicFolderPath) );
+
+app.use( express.static(path.join(__dirname, '/views')));
+app.use( express.static(path.join(__dirname, '/public')));
+
+
+
+app.listen (3000, () => {
+        console.log('Servidor corriendo')
+});
+
+app.get('/index.html', (req,res) => {
+        res.sendFile(path.resolve(__dirname + '/views/home.html'));
+});
+
+app.get('/register.html', (req,res) => {
+        res.sendFile(path.resolve(__dirname + '/views/register.html'));
+});
+
+app.get('/login.html', (req,res) => {
+        res.sendFile(path.resolve(__dirname + '/views/login.html'));
+});
+
+app.get('*', (req,res) => {
+    res.send("Error, Pagina mal ingresada");
+});
+
